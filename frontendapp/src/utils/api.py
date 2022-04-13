60bb69e257c9dd777ee51414bb8f29ba3f92b200
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-04-13 02:03:25 -0500

    NUEVOS CAMBIOS EN DOCKER-COMPOSE.YML Y DOCKERFILE, API.PY

--- frontendapp/src/utils/api.py
+++ frontendapp/src/utils/api.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def mypost(id:int,mensaje:str):
-    network = 'backend-network:8000'
+    network = 'backend:8000'
     url = 'http://{}/items/{}?mensaje={}'.format(network,id,mensaje)
     response = requests.get(url)
     response = json.loads(response.content)
