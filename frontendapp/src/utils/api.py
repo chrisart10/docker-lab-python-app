8d3e4b1c3e8282d5b545cd954f4d6ca5777e5e93
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-04-13 02:35:00 -0500

    NUEVO TAG PARA EL DOCKERFILE, SE CORRIGIO DEL FRONTEND API.PY

--- frontendapp/src/utils/api.py
+++ frontendapp/src/utils/api.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def mypost(id:int,mensaje:str):
-    network = 'backend:5001'
+    network = 'backend:80'
     url = 'http://{}/items/{}?mensaje={}'.format(network,id,mensaje)
     response = requests.get(url)
     response = json.loads(response.content)
